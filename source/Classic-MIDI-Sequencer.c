e08b0a32191e0040f933de9ff0cde06e9021820f
BramGiesen <giesenbram@gmail.com>
2018-12-08 14:51:38 +0100

    Added record overwrite mode

--- source/Classic-MIDI-Sequencer.c
+++ source/Classic-MIDI-Sequencer.c
@@ -40,6 +40,7 @@ typedef enum {
 typedef struct {
 
   //syncVars=================================================
+  
   LV2_URID_Map*  map;     // URID map feature
   MetroURIs      uris;    // Cache of mapped URIDs
   LV2_Atom_Sequence* control;
@@ -48,29 +49,25 @@ typedef struct {
   float bpm;
   float speed; // Transport speed (usually 0=stop, 1=play)
   float phase;
+  
   //==========================================================
   
-  // was last run on the 2nd output?
-  bool was_second;
   bool playing;
 
   // URIDs
   LV2_URID urid_midiEvent;
 
-  // aray to save midi events
-  //uint8_t midiEventsOn[64];
   
 	uint8_t *midiEventsOn;
   size_t used;
   size_t size; 
   
 	const float* mode;
-  // control ports
-  const float* port_target;
 
   // atom ports
   const LV2_Atom_Sequence* port_events_in;
   LV2_Atom_Sequence* port_events_out1;
+
 } Data;
 
 
@@ -107,7 +104,6 @@ static LV2_Handle instantiate(const LV2_Descriptor*     descriptor,
     self->urid_midiEvent = map->map(map->handle, LV2_MIDI__MidiEvent);
 
     MetroURIs* const    uris  = &self->uris;
-    //LV2_URID_Map* const map   = self->map;
     uris->atom_Blank          = map->map(map->handle, LV2_ATOM__Blank);
     uris->atom_Float          = map->map(map->handle, LV2_ATOM__Float);
     uris->atom_Object         = map->map(map->handle, LV2_ATOM__Object);
@@ -118,7 +114,7 @@ static LV2_Handle instantiate(const LV2_Descriptor*     descriptor,
     uris->time_barBeat        = map->map(map->handle, LV2_TIME__barBeat);
     uris->time_beatsPerMinute = map->map(map->handle, LV2_TIME__beatsPerMinute);
     uris->time_speed          = map->map(map->handle, LV2_TIME__speed);
-   // Initialise instance fields
+   
     self->rate       = rate;
     self->bpm        = 120.0f;
      
@@ -133,7 +129,8 @@ static LV2_Handle instantiate(const LV2_Descriptor*     descriptor,
 
 
 
-static void connect_port(LV2_Handle instance, uint32_t port, void* data)
+static void 
+connect_port(LV2_Handle instance, uint32_t port, void* data)
 {
   Data* self = (Data*)instance;
 
@@ -155,17 +152,19 @@ static void connect_port(LV2_Handle instance, uint32_t port, void* data)
 
 
 
-static void activate(LV2_Handle instance)
+static void 
+activate(LV2_Handle instance)
 {
-   // Data* self = (Data*)instance;
+
 }
 
 
 //phase oscillator to use for timing of the beatsync 
-static float* phaseOsc(float frequency, float* phase, float rate)
+static float* 
+phaseOsc(float frequency, float* phase, float rate)
 {
   *phase += frequency / rate;
-  //wrap phase 
+  
   if(*phase >= 1) *phase = *phase - 1; 
   
   return phase;
@@ -173,7 +172,8 @@ static float* phaseOsc(float frequency, float* phase, float rate)
 
 
 //create a midi message 
-static LV2_Atom_MIDI createMidiEvent(Data* self, uint8_t status, uint8_t note, uint8_t velocity)
+static LV2_Atom_MIDI 
+createMidiEvent(Data* self, uint8_t status, uint8_t note, uint8_t velocity)
 { 
   LV2_Atom_MIDI msg;
   memset(&msg, 0, sizeof(LV2_Atom_MIDI));
@@ -189,7 +189,10 @@ static LV2_Atom_MIDI createMidiEvent(Data* self, uint8_t status, uint8_t note, u
 }
 
 
-static void insertNote(Data* self, uint8_t note) {
+
+static void 
+insertNote(Data* self, uint8_t note) 
+{ 
   if (self->used == self->size) {
     self->size *= 2;
     self->midiEventsOn = (uint8_t *)realloc(self->midiEventsOn, self->size * sizeof(uint8_t));
@@ -197,14 +200,21 @@ static void insertNote(Data* self, uint8_t note) {
   self->midiEventsOn[self->used++] = note;
 }
 
-static void clearSequence(Data* self)
+
+
+static void 
+clearSequence(Data* self)
 {
   free(self->midiEventsOn);
   self->midiEventsOn = NULL;
   self->used = self->size = 0;
 }
+
+
+
 //sequence the MIDI notes that are written into an array
-static void sequence(Data* self)
+static void 
+sequence(Data* self)
 {
   static bool first = false;
   static bool trigger = false;
@@ -217,7 +227,6 @@ static void sequence(Data* self)
   // Write an empty Sequence header to the outputs
   lv2_atom_sequence_clear(self->port_events_out1);
 
-  // LV2 is so nice...
   self->port_events_out1->atom.type = self->port_events_in->atom.type;
 
   if (self->playing == true) {
@@ -247,7 +256,7 @@ static void sequence(Data* self)
     }
   } else { // self->playing = false, send note offs of current notes.
     
-    if ( !cleared && *self->mode == 1 ) {
+    if ( !cleared && *self->mode < 2 ) {
       printf("clearing notes\n"); 
       for (size_t i = 0; i < self->used; i++) {
         LV2_Atom_MIDI msg = createMidiEvent(self, 128, self->midiEventsOn[i], 0);
@@ -260,6 +269,8 @@ static void sequence(Data* self)
     }
   }
 }
+
+
 
 static void
 update_position(Data* self, const LV2_Atom_Object* obj)
@@ -285,9 +296,6 @@ update_position(Data* self, const LV2_Atom_Object* obj)
     self->speed = ((LV2_Atom_Float*)speed)->body;
   }
   if (beat && beat->type == uris->atom_Float) {
-    // Received a beat position, synchronise
-    // This hard sync may cause clicks, a real plugin would be more graceful
-    // const float frames_per_beat = 60.0f / self->bpm * self->rate;
     const float bar_beats       = ((LV2_Atom_Float*)beat)->body;
     const float beat_beats      = bar_beats - floorf(bar_beats);  
    
@@ -295,33 +303,33 @@ update_position(Data* self, const LV2_Atom_Object* obj)
 
       self->phase = beat_beats;
       previousSpeed = self->speed;
-     // printf("beat_beats = %f\n", beat_beats);
-     // printf("phase = %f\n", self->phase);  
     }
   }
 }
 
 
-static void run(LV2_Handle instance, uint32_t n_samples)
+
+static void 
+run(LV2_Handle instance, uint32_t n_samples)
 {
     Data* self = (Data*)instance;
     static float prevMod = 1;
     static bool recording = false;
 
-    //switch between the play/recording modes ====
-
+    //TODO create seperate function for the switching
+    //switch between the play/recording modes =========
+  
     int modeStatus = (int)*self->mode;
 
     if (*self->mode != prevMod) {
       switch (modeStatus)
       {
         case 0:
-          printf("clear all\n");
+          self->playing = false;
           break;
         case 1:
           printf("record\n");
           self->playing = false;
-          //TODO need to clear array here
           recording = true;
           break;
         case 2:
@@ -364,12 +372,19 @@ static void run(LV2_Handle instance, uint32_t n_samples)
         const uint8_t* const msg = (const uint8_t*)(ev + 1);
         const uint8_t status  = msg[0] & 0xF0;
 
+        static size_t count = 0;
+
         switch (status)
         {
           case LV2_MIDI_MSG_NOTE_ON:
             if (recording == true){
-              insertNote(self, msg[1]);
-            }
+
+              if (*self->mode == 1 || *self->mode == 3) {
+                insertNote(self, msg[1]);
+              } else {
+                self->midiEventsOn[count++ % self->used] = msg[1];
+              }
+            } 
             break;
           default:
             break;
@@ -384,7 +399,7 @@ static void run(LV2_Handle instance, uint32_t n_samples)
 
 		 // Work forwards in time frame by frame, handling events as we go
 		 const LV2_Atom_Sequence* in     = self->control;
-		 uint32_t                 last_t = 0;
+		 
 		 for (const LV2_Atom_Event* ev = lv2_atom_sequence_begin(&in->body);
 				 !lv2_atom_sequence_is_end(&in->body, in->atom.size, ev);
 				 ev = lv2_atom_sequence_next(ev)) {
